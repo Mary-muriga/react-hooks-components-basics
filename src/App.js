9398c814f0cb95ad9287276a3343a091853cfeea
Marymuriga <marymuriga@gmail.com>
2022-07-12 09:38:34 +0300

    Done.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Comment from "./Comment";
 function App() {
   return (
     <div>
-      <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div> 
-      <div>Naturally, I agree with this article.</div>
+     <Article />
+      <Comment />
     </div>
   );
 }
